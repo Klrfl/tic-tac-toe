f007bcdc9a4a9e77935a5eb57e5694b54c98736f
Efraim Munthe <efrayanglain@gmail.com>
2023-12-04 20:33:12 +0700

    add className prop

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,8 @@
 import "./Square.css";
 
-export default function Square({ value, onSquareClick }) {
+export default function Square({ value, onSquareClick, className = "" }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className={`square ${className}`} onClick={onSquareClick}>
       {value}
     </button>
   );
